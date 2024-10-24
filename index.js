0312d75db2398bde7d1fd9dcdf3e40b15510c1db
mehmet.tokmak <mehmet.tokmak@beyn.com.tr>
2024-10-24 12:13:14 +0300

    .

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const request = require("request");
+const axios = require("axios");
 
-getLocal = () => {
-  return new Promise((resolve, reject) => {
-    (async () => {
-      var options = {
-        method: "POST",
-        url: "https://bo-api-staging.xpress-ix.com/sysapi/v1/account/search/filters",
+getLocal = async (url) => {
+  const startTime = Date.now();
+
+  try {
+    const response = await axios.post(
+      `https://${url}.xpress-ix.com/sysapi/v1/account/search/filters`,
+      {
+        start: "0",
+        end: "25",
+        length: "25",
+        page: "1",
+        order: "id",
+        orderType: "asc",
+        boToken: "9711fc0274f7ce5a438f18ad84ae71c3",
+        external_login: "null",
+      },
+      {
         headers: {
           accept: "application/json, text/plain, */*",
           "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -25,30 +36,23 @@ getLocal = () => {
           "user-agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
         },
-        form: {
-          start: "0",
-          end: "25",
-          length: "25",
-          page: "1",
-          order: "id",
-          orderType: "asc",
-          boToken: "9711fc0274f7ce5a438f18ad84ae71c3",
-          external_login: "null",
-        },
-      };
-      request(options, function (error, response) {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(response.body);
-        }
-      });
-    })();
-  });
+      }
+    );
+
+    const endTime = Date.now();
+    const responseTime = endTime - startTime;
+
+    return { response, responseTime };
+  } catch (error) {
+    throw error;
+  }
 };
 
 (async () => {
-
-    const result = await getLocal();
-    console.log({result});
+  try {
+    const { response, responseTime } = await getLocal("mehmet-bo-api-dev");
+    console.log({ responseTime, responseBody: response.data });
+  } catch (error) {
+    console.error(error);
+  }
 })();
